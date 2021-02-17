d590254965004362e7c8c2d6056c9292fd805d5d
sowens-csd <stephen.owens@thoughtwire.com>
2021-02-17 11:21:17 -0500

    fix: use localeId for web lang #107

--- speech_to_text/lib/speech_to_text_web.dart
+++ speech_to_text/lib/speech_to_text_web.dart
@@ -134,6 +134,7 @@ class SpeechToTextPlugin extends SpeechToTextPlatform {
     _webSpeech.onresult = allowInterop(_onResult);
     _webSpeech.interimResults = partialResults;
     _webSpeech.continuous = partialResults;
+    _webSpeech.lang = localeId;
     _webSpeech.start();
     return true;
   }
