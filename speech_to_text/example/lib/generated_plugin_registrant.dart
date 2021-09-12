02bfcaff9d999d04725e4b4af002051067a03ac7
sowens-csd <stephen.owens@thoughtwire.com>
2021-09-12 12:18:54 -0400

    feat: example upgrade to support web

---
+++ speech_to_text/example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,15 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:speech_to_text/speech_to_text_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  SpeechToTextPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
