4611e84bfeaeefda0cfba178e00a91f42398df22
sowens-csd <stephen.owens@thoughtwire.com>
2021-09-18 08:50:00 -0400

    rf: automatic updates

--- speech_to_text/example/lib/generated_plugin_registrant.dart
+++ speech_to_text/example/lib/generated_plugin_registrant.dart
@@ -2,6 +2,7 @@
 // Generated file. Do not edit.
 //
 
+// ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:speech_to_text/speech_to_text_web.dart';
