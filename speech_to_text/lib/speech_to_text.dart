5cdc706b4d419ecbc786565b841665431b07b25b
Ievgenii Kovtun <ievgenii.kovtun@veeam.com>
2023-06-19 02:20:20 +0200

    Add setPauseFor method for late setup

--- speech_to_text/lib/speech_to_text.dart
+++ speech_to_text/lib/speech_to_text.dart
@@ -436,6 +436,16 @@ class SpeechToText {
       throw ListenFailedException(e.message, e.details, e.stacktrace);
     }
   }
+  void setPauseFor(Duration pauseFor) {
+    //Setup new pauseFor only if listen is active and pauseFor is different
+    if ((_listenTimer?.isActive ?? false) && (_pauseFor == null || _pauseFor!.compareTo(pauseFor) != 0)) {
+      _listenTimer?.cancel();
+      _listenTimer = null;
+      _setupListenAndPause(pauseFor, _listenFor);
+    } else {
+      throw ListenNotStartedException();
+    }
+  }
 
   void _setupListenAndPause(
       Duration? initialPauseFor, Duration? initialListenFor) {
@@ -659,3 +669,5 @@ class ListenFailedException implements Exception {
   final String? stackTrace;
   ListenFailedException(this.message, [this.details, this.stackTrace]);
 }
+
+class ListenNotStartedException implements Exception {}
