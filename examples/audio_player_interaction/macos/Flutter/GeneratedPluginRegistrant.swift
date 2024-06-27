6e4f2288dc2c1c729be46b6e0e11889a85e5d57b
Alex Rabin <alex.rabin@sentracam.com>
2024-06-27 14:00:49 -0700

    Beginning of darwin implementation

--- examples/audio_player_interaction/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/audio_player_interaction/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import path_provider_foundation
 import speech_to_text
 import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
