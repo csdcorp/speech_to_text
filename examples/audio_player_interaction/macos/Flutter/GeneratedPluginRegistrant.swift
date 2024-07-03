f689babf610677d17bd517b6eb94551972ebf0dd
Alex Rabin <alex.rabin@sentracam.com>
2024-07-03 12:27:18 -0700

    Updated support for macos for the audio_player_interaction

--- examples/audio_player_interaction/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/audio_player_interaction/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,9 @@ import Foundation
 import audioplayers_darwin
 import path_provider_foundation
 import speech_to_text
-import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
