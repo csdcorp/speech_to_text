88856b97a31ce30b6f31623953c7cdb298c84a75
sowens-csd <stephen.owens@thoughtwire.com>
2024-08-12 14:46:19 -0400

    feat: latest MacOS dependencies

--- speech_to_text/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ speech_to_text/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import speech_to_text
-import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
